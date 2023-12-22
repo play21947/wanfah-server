44626eb489784e9b89b3b10efafc902b4744cf60
P1ay2 <66400989+play21947@users.noreply.github.com>
2023-12-22 21:56:51 +0700

    change database wanfah

--- config.js
+++ config.js
@@ -8,29 +8,15 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-// Wanfah
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBKV4CqTg78DhozcUFu_et4lp711GoTNuA",
-//   authDomain: "wanfah-lottery.firebaseapp.com",
-//   databaseURL: "https://wanfah-lottery-default-rtdb.asia-southeast1.firebasedatabase.app",
-//   projectId: "wanfah-lottery",
-//   storageBucket: "wanfah-lottery.appspot.com",
-//   messagingSenderId: "761993847576",
-//   appId: "1:761993847576:web:1169870a8f60114af7746d",
-//   measurementId: "G-YW5FKN3ZT1"
-// };
-
-// OTPPROJECT
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAZ4S5iIyDgegUUDiDfrEP8pXGYq0mcaFg",
-  authDomain: "project-otp-1ad68.firebaseapp.com",
-  databaseURL: "https://project-otp-1ad68-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "project-otp-1ad68",
-  storageBucket: "project-otp-1ad68.appspot.com",
-  messagingSenderId: "39744714975",
-  appId: "1:39744714975:web:9aca89d040faf8708ed16a",
-  measurementId: "G-G1D80ZJY5M"
+  apiKey: "AIzaSyBKV4CqTg78DhozcUFu_et4lp711GoTNuA",
+  authDomain: "wanfah-lottery.firebaseapp.com",
+  databaseURL: "https://wanfah-lottery-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wanfah-lottery",
+  storageBucket: "wanfah-lottery.appspot.com",
+  messagingSenderId: "761993847576",
+  appId: "1:761993847576:web:1169870a8f60114af7746d",
+  measurementId: "G-YW5FKN3ZT1"
 };
 
 
